e3110277f3e8ff79d5aeb53c75e1c3668779e91e
Conor Callahan <conorcallahan97@outlook.com>
2024-04-22 21:15:50 -0500

    Got the firtst function working and entered my six albums

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,13 +1,30 @@
 console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
 
+myCollection = [];
+ 
+function addToCollection(collection, title, artist, yearPublished){
+const newAlbum = {
+  title: title,
+  artiist: artist,
+  yearPublished: yearPublished
+};
+
+  collection.push(newAlbum);
+   
+  return newAlbum;
+}
+
+let albumOne = addToCollection(myCollection, "Days Of Future Passed", "The Moody Blues", "1967");
+let albumTwo = addToCollection(myCollection, "Morning View", "Incubus", "2001");
+let albumThree = addToCollection(myCollection, "Planet Piano", "DR.GABBA", "2022");
+let albumFour = addToCollection(myCollection, "The Richest Man In Babylon", "Thievery Corporation", "2003");
+let albumFive = addToCollection(myCollection, "Brasilian Skies", "Masayoshi Takanaka", "1978");
+let albumSix = addToCollection(myCollection, "2", "Mac Demarco", "2012");
 
 
 
-
-
-
-
+console.log(myCollection);
 
 // PLEASE DO NOT MODIFY THIS. Just leave it down here at the bottom. Think of it
 // as a lil' chunk of friendly code that you don't need to understand right now.
